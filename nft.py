1808ee82bd535e5582ec720465d9a0a42cda9f3f
Sharan S <skiller.sharan@gmail.com>
2024-01-27 19:22:42 +0530

    Add time taken

--- nft.py
+++ nft.py
@@ -4,17 +4,25 @@ from PIL import Image, ImageDraw
 from datetime import datetime
 from tqdm import tqdm  # Import tqdm for progress bar
 
+def collect_trait_images(trait_folder, supported_formats):
+    trait_images = []
+    trait_name = os.path.basename(trait_folder)
+
+    # Collect images for each trait
+    for file in os.listdir(trait_folder):
+        if any(file.lower().endswith(format) for format in supported_formats):
+            image_path = os.path.join(trait_folder, file)
+            img = Image.open(image_path)
+            trait_images.append((trait_name, img))  # Store both trait name and image object
+
+    return trait_images
+
 def generate_characters(input_folder, output_parent_folder, supported_formats):
     # Collect trait folders
     trait_folders = [f.path for f in sorted(os.scandir(input_folder), key=lambda x: int(x.name.split("_")[0])) if f.is_dir()]
 
-    # Initialize trait combinations
-    trait_combinations = [[]]
-
     # Count the total number of traits to be generated
-    total_traits = 0
-    for trait_folder in trait_folders:
-        total_traits += len([file for file in os.listdir(trait_folder) if any(file.lower().endswith(format) for format in supported_formats)])
+    total_traits = sum(len([file for file in os.listdir(trait_folder) if any(file.lower().endswith(format) for format in supported_formats)]) for trait_folder in trait_folders)
 
     # Generate a timestamped folder for output
     timestamp_folder = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -22,25 +30,21 @@ def generate_characters(input_folder, output_parent_folder, supported_formats):
     os.makedirs(output_folder, exist_ok=True)
 
     # Set up tqdm progress bar
-    print("Total traits : " + str(total_traits))
+    print("Total traits: " + str(total_traits))
+    # Initialize trait combinations
+    trait_combinations = [[]]
 
     # Iterate over trait folders
     for trait_folder in trait_folders:
-        trait_images = []
-        trait_name = os.path.basename(trait_folder)
-        # Collect images for each trait
-        for file in os.listdir(trait_folder):
-            if any(file.lower().endswith(format) for format in supported_formats):
-                image_path = os.path.join(trait_folder, file)
-                img = Image.open(image_path)
-                trait_images.append((trait_name, img))  # Store both trait name and image object
+        trait_images = collect_trait_images(trait_folder, supported_formats)
 
         # Update trait combinations with new trait images
-        trait_combinations = [combo + [(trait_name, img)] for combo in trait_combinations for trait_name, img in trait_images]
+        trait_combinations = [combo + [trait_image] for combo in trait_combinations for trait_image in trait_images]
 
 
     # Set up tqdm progress bar for character generation
     progress_bar = tqdm(total=len(trait_combinations), desc="Generating Characters", unit="character")
+    start_time = datetime.now()
 
     # Generate characters based on trait combinations
     for i, combination in enumerate(trait_combinations):
@@ -57,8 +61,7 @@ def generate_characters(input_folder, output_parent_folder, supported_formats):
 
         # Create JSON file for trait information
         json_data = {"character_name": character_name}
-        json_data.update({trait_name.split("_", 1)[1]: os.path.splitext(os.path.basename(img.filename))[0] for trait_name, img in combination})
-        
+        json_data.update({trait_name.split("_", 1)[1].replace(" ", "_").lower(): os.path.splitext(os.path.basename(img.filename))[0].replace(" ", "_").lower() for trait_name, img in combination})
         # Save trait information as JSON
         json_path = os.path.join(output_folder, f"{character_name}_traits.json")
         with open(json_path, 'w') as json_file:
@@ -70,7 +73,10 @@ def generate_characters(input_folder, output_parent_folder, supported_formats):
     # Close the tqdm progress bar
     progress_bar.close()
 
-    print("Completed !")
+    end_time = datetime.now()
+    total_time = (end_time - start_time).total_seconds()
+
+    print(f"Completed in {total_time:.2f} seconds")
 
 if __name__ == "__main__":
     # Config variables
